9ea199441b8dd00c3d032baab5b4fb3b2747115d
Felipe Balbi <felipe@balbi.sh>
2022-09-01 10:05:39 +0300

    volume2: chapter13: Add chapter13 files
    There's a bug somewhere which prevents the game from actually starting
    
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

---
+++ volume2/chapter13/gameHUD.asm
@@ -0,0 +1,325 @@
+;; =============================================================================
+;; Constants
+
+HUDEnergyMax			= 7
+HUDDrinksMax			= 9
+HUDTowelsMax			= 9
+HUDScoreIncrease		= 5
+HUDEnergyColumn			= 1
+HUDBarCustomersColumn		= 11
+HUDLoungersCustomersColumn	= 13
+HUDNumDrinksColumn		= 17
+HUDNumTowelsColumn		= 21
+HUDDrinkCarryingColumn		= 9
+HUDDrinkCarryingChar		= 81
+HUDDrinkNotCarryingChar		= 104
+HUDScoreColumn1			= 29
+HUDScoreColumn2			= 30
+HUDScoreColumn3			= 31
+HUDHiScoreColumn1		= 36
+HUDHiScoreColumn2		= 37
+HUDHiScoreColumn3		= 38
+HUDRow				= 2
+HUDStartColumn			= 10
+HUDStartRow			= 24
+
+;; =============================================================================
+;; Variables
+
+bHudEnergy:			!byte HUDEnergyMax
+bHudBarCustomers:		!byte 0
+bHudLoungersCustomers:		!byte 0
+bHudNumDrinks:			!byte HUDDrinksMax
+bHudNumTowels:			!byte HUDTowelsMax
+bHudDrinkCarrying:		!byte WHITE
+wHudScore:			!word 0
+wHudHiScore:			!word 0
+tHudStartText:			!text "press fire to start"
+				!byte 0
+tHudStartClearText:		!text "			  "
+				!byte 0
+
+;; =============================================================================
+;; Subroutines
+
+gameHUDInit:
+	lda #HUDEnergyMax
+	sta bHudEnergy
+	lda #HUDDrinksMax
+	sta bHudNumDrinks
+	lda #HUDTowelsMax
+	sta bHudNumTowels
+	lda #0
+	sta bHudBarCustomers
+	sta bHudLoungersCustomers
+	sta wHudScore+1
+	sta wHudScore
+	rts
+
+;; =============================================================================
+
+gameHUDUpdate:
+	jsr gameHUDUpdateEnergy
+	jsr gameHUDUpdateBarCustomers
+	jsr gameHUDUpdateLoungersCustomers
+	jsr gameHUDUpdateNumDrinks
+	jsr gameHUDUpdateNumTowels
+	jsr gameHUDUpdateDrinksCarrying
+	jsr gameHUDUpdateScore
+	jsr gameHUDUpdateHiScore
+	rts
+
+;; =============================================================================
+
+gameHUDUpdateEnergy:
+	ldx #0
+	lda #HUDEnergyColumn
+	sta ZeroPage9
+gHUHLoop:
+	txa
+	cmp bHudEnergy
+	bcs gHUHRed
+	+LIBSCREEN_SETCOLOR_S_AVV ZeroPage9, HUDRow, GREEN
+	jmp gHUHEnd
+
+gHUHRed:
+	+LIBSCREEN_SETCOLOR_S_AVV ZeroPage9, HUDRow, RED
+
+gHUHEnd:
+	inc ZeroPage9
+	inx
+	cpx #HUDEnergyMax
+	bne gHUHLoop
+	rts
+
+;; =============================================================================
+
+gameHUDUpdateBarCustomers:
+	lda bHudBarCustomers			; Load number of bar customers
+	bne gHUBCRed				; If not 0, skip to red
+
+	;; Set icon to green
+	+LIBSCREEN_SETCOLOR_S_VVV HUDBarCustomersColumn, HUDRow, GREEN
+	jmp gHUBCEnd				; Skip to end
+gHUBCRed:
+	;; Set icon to red
+	+LIBSCREEN_SETCOLOR_S_VVV HUDBarCustomersColumn, HUDRow, RED
+gHUBCEnd:
+	rts
+
+;; =============================================================================
+
+gameHUDUpdateLoungersCustomers:
+	lda bHudLoungersCustomers
+	bne gHULCRed
+	+LIBSCREEN_SETCOLOR_S_VVV HUDLoungersCustomersColumn, HUDRow, GREEN
+	jmp gHULCEnd
+
+gHULCRed:
+	+LIBSCREEN_SETCOLOR_S_VVV HUDLoungersCustomersColumn, HUDRow, RED
+
+gHULCEnd:
+	rts
+
+;; =============================================================================
+
+gameHUDUpdateNumDrinks:
+	lda bHudNumDrinks
+	ora #$30				; Convert to ascii
+	sta ZeroPage9
+	+LIBSCREEN_SETCHARACTER_S_VVA HUDNumDrinksColumn, HUDRow, ZeroPage9
+	rts
+
+;; =============================================================================
+
+gameHUDUpdateNumTowels:
+	lda bHudNumTowels			; Convert to ascii
+	ora #$30
+	sta ZeroPage9
+	+LIBSCREEN_SETCHARACTER_S_VVA HUDNumTowelsColumn, HUDRow, ZeroPage9
+	rts
+
+;; =============================================================================
+
+gameHUDUpdateDrinksCarrying:
+	;; Player drinks carrying
+	+LIBSCREEN_SETCOLOR_S_VVA HUDDrinkCarryingColumn, HUDRow, bHudDrinkCarrying
+	+LIBSCREEN_SETCHARACTER_S_VVV HUDDrinkCarryingColumn, HUDRow, HUDDrinkCarryingChar
+	lda bHudDrinkCarrying
+	cmp #WHITE
+	bne gHUDCNotCarryingADrink
+	+LIBSCREEN_SETCHARACTER_S_VVV HUDDrinkCarryingColumn, HUDRow, HUDDrinkNotCarryingChar
+
+gHUDCNotCarryingADrink:
+	rts
+
+;; =============================================================================
+
+gameHUDUpdateScore:
+	;; -------- 1st digit --------
+	lda wHudScore+1
+
+	;; get low nibble
+	and #$0f
+
+	;; convert to ascii
+	ora #$30
+	sta ZeroPage9
+
+	+LIBSCREEN_SETCHARACTER_S_VVA HUDScoreColumn1, HUDRow, ZeroPage9
+
+	;; -------- 2nd digit --------
+	lda wHudScore
+
+	;; get high nibble
+	and #$f0
+
+	;; convert to ascii
+	lsr
+	lsr
+	lsr
+	lsr
+	ora #$30
+	sta ZeroPage9
+
+	+LIBSCREEN_SETCHARACTER_S_VVA HUDScoreColumn2, HUDRow, ZeroPage9
+
+	;; -------- 3rd digit --------
+	lda wHudScore
+
+	;; get low nibble
+	and #$0f
+
+	;; convert to ascii
+	ora #$30
+	sta ZeroPage9
+
+	+LIBSCREEN_SETCHARACTER_S_VVA HUDScoreColumn3, HUDRow, ZeroPage9
+
+	rts
+
+;; =============================================================================
+
+gameHUDUpdateHiScore:
+	;; -------- 1st digit --------
+	lda wHudHiScore+1
+
+	;; get low nibble
+	and #$0f
+
+	;; convert to ascii
+	ora #$30
+	sta ZeroPage9
+
+	+LIBSCREEN_SETCHARACTER_S_VVA HUDHiScoreColumn1, HUDRow, ZeroPage9
+
+	;; -------- 2nd digit --------
+	lda wHudHiScore
+
+	;; get high nibble
+	and #$f0
+
+	;; convert to ascii
+	lsr
+	lsr
+	lsr
+	lsr
+	ora #$30
+	sta ZeroPage9
+
+	+LIBSCREEN_SETCHARACTER_S_VVA HUDHiScoreColumn2, HUDRow, ZeroPage9
+
+	;; -------- 3rd digit --------
+	lda wHudHiScore
+
+	;; get low nibble
+	and #$0f
+
+	;; convert to ascii
+	ora #$30
+	sta ZeroPage9
+
+	+LIBSCREEN_SETCHARACTER_S_VVA HUDHiScoreColumn3, HUDRow, ZeroPage9
+	rts
+
+;; =============================================================================
+
+gameHUDDecreaseEnergy:
+	lda bHudEnergy
+	beq gHDHEnd
+	dec bHudEnergy
+
+gHDHEnd:
+	rts
+
+;; =============================================================================
+
+gameHUDDecreaseNumDrinks:
+	lda bHudNumDrinks
+	beq gHDNDEnd
+	dec bHudNumDrinks
+
+gHDNDEnd:
+	rts
+
+;; =============================================================================
+
+gameHUDFillDrinks:
+	lda #HUDDrinksMax
+	sta bHudNumDrinks
+	rts
+
+;; =============================================================================
+
+gameHUDDecreaseNumTowels:
+	lda bHudNumTowels
+	beq gHDNTEnd
+	dec bHudNumTowels
+
+gHDNTEnd:
+	rts
+
+;; =============================================================================
+
+gameHUDFillTowels:
+	lda #HUDTowelsMax
+	sta bHudNumTowels
+	rts
+
+;; =============================================================================
+
+gameHUDIncreaseScore:
+	sed					; Set decimal mode
+	+LIBMATH_ADD16BIT_AVA wHudScore, HUDScoreIncrease, wHudScore
+	cld					; Clear decimal mode
+	rts
+
+;; =============================================================================
+
+gameHUDClearPressStart:
+	+LIBSCREEN_DRAWTEXT_VVA HUDStartColumn, HUDStartRow, tHudStartClearText
+	rts
+
+;; =============================================================================
+
+gameHUDShowPressStart:
+	+LIBSCREEN_DRAWTEXT_VVA HUDStartColumn, HUDStartRow, tHudStartText
+	rts
+
+;; =============================================================================
+
+gameHUDCalculateNewHiScore:
+	lda wHudScore
+	cmp wHudHiScore
+	lda wHudScore+1
+	sbc wHudHiScore+1
+
+	bcc gHCNotHi
+
+	lda wHudScore
+	sta wHudHiScore
+	lda wHudScore+1
+	sta wHudHiScore+1
+
+gHCNotHi:
+	rts
---
+++ volume2/chapter13/gameMain.asm
@@ -0,0 +1,94 @@
+!cpu 6510
+!convtab scr
+
+;;; ============================================================================
+;;; 		   RetroGameDev C64 Edition Volume 2 Chapter 13
+;;; ============================================================================
+;;; Basic Loader
+
+*= $0801
+
+!macro basic_loader .lineno, .loadaddr {
+	!word @end	    ; Next basic line
+	!word .lineno	    ; Line number
+	!byte $9e	    ; SYS
+	!byte '0' + (.loadaddr % 100000 / 10000)
+	!byte '0' + (.loadaddr % 10000 / 1000)
+	!byte '0' + (.loadaddr % 1000 / 100)
+	!byte '0' + (.loadaddr % 100 / 10)
+	!byte '0' + (.loadaddr % 10)
+	!byte $00, $00, $00 ; Terminator
+@end:
+}
+
+	+basic_loader 2022, gameMainInit
+
+;;; ============================================================================
+;;; Includes
+
+!src "libIncludes.asm"
+!src "gameData.asm"
+!src "gamePlayer.asm"
+!src "gameHUD.asm"
+!src "gameBar.asm"
+!src "gameLoungers.asm"
+!src "gameCrabs.asm"
+!src "gameFlow.asm"
+
+;;; ============================================================================
+;;; Constants
+
+IrqFast				= True
+Irq1ScanLine			= 10
+Irq2ScanLine			= 140
+
+;;; ============================================================================
+;;; Initialize
+
+gameMainInit:
+	+LIBUTILITY_DISABLEBASICANDKERNAL		; Disable BASIC and KERNAL RORMs
+	+LIBUTILITY_SET1000_AV SCREENRAM, Space		; Clear the screen
+	+LIBSCREEN_SETSCREENCOLOR_V YELLOW		; Set the screen color
+
+	+LIBSPRITE_MULTICOLORENABLEALL_V True		; Set the sprite multicolor mode
+	+LIBSPRITE_SETMULTICOLORS_VV LIGHT_RED, BROWN	; Set the sprite multicolors
+	+LIBSCREEN_SETMULTICOLORMODE_V True		; Set the background multicolor mode
+	+LIBSCREEN_SETMULTICOLORS_VV BLACK, BROWN	; Set the background multicolors
+	+LIBSCREEN_SETCHARMEMORY_V CharacterSlot2000	; Set the custom charset
+	+LIBSCREEN_SETBACKGROUND_AA gameDataBackground + PlayerScreenTopLeft*1000, gameDataBackgroundCol ; Set the background screen
+
+	+LIBMATH_RANDSEED_AA bMathRandomCurrent1, TIMALO ; Seed the random number lists
+	+LIBMATH_RANDSEED_AA bMathRandomCurrent2, TIMALO
+
+	+LIBSOUND_INIT_A gameDataSID			; Initialize the sound
+	+LIBRASTERIRQ_INIT_VAV Irq1ScanLine, gameMainIRQ1, IrqFast ; Initialize IRQ
+
+	jsr gamePlayerInit				; Call the player initialization subroutine
+	jsr gameBarInit					; Call the bar initialize subroutine
+	jsr gameLoungersInit				; Call the loungers initialize subroutine
+
+;;; ============================================================================
+;;; Update
+
+gameMainUpdate:
+	+LIBSCREEN_WAIT_V 250				; Wait for scanline 250
+	+LIBSPRITE_UPDATE				; Update the sprites
+	jsr gameFlowUpdate				; Update the game flow subroutines
+	jsr gameHUDUpdate				; Update the HUD subroutines
+	jmp gameMainUpdate				; Jump back, infinite loop
+
+;;; ============================================================================
+;;; IRQ Handlers
+
+gameMainIRQ1:
+	+LIBRASTERIRQ_START_V IrqFast 			; Start the IRQ
+	+LIBSOUND_UPDATE_A gameDataSID			; Update the sound player
+	jsr gameCrabsUpdateTop
+	+LIBRASTERIRQ_SET_VAV Irq2ScanLine, gameMainIRQ2, IrqFast ; Point to 2nd IRQ
+	+LIBRASTERIRQ_END_V IrqFast			; End the IRQ
+
+gameMainIRQ2:
+	+LIBRASTERIRQ_START_V IrqFast 			; Start the IRQ
+	jsr gameCrabsUpdateBottom
+	+LIBRASTERIRQ_SET_VAV Irq1ScanLine, gameMainIRQ1, IrqFast ; Point to the 1st IRQ
+	+LIBRASTERIRQ_END_V IrqFast			; End the IRQ
